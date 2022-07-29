0c9cbc1f2338f5893d0d69ad327fcd85e4c4751d
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-07-28 22:10:27 -0300

    adicionando propriedades no objeto

---
+++ adc-campos.js
@@ -0,0 +1,18 @@
+const cliente = { 
+    nome: "Jay",
+    idade: 63,
+    cpf: "34589756900",
+    email: "pritchettclosets@email.com",
+}
+console.log(cliente);
+
+//atribuindo campo telefone no objeto cliente
+cliente.telefone = "3030-2020"
+
+console.log(cliente);
+
+//alterando número de telefone - substituindo o valor anterior
+cliente.telefone = "2020-3030"
+
+console.log(cliente);
+
