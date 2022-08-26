58a224f8f93939bd00127277e9befbecb6b40356
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-08-25 23:15:28 -0300

    adicionando um objeto em outro com notação de ponto.

---
+++ objetos-em-objetos.js
@@ -0,0 +1,17 @@
+const cliente = { 
+    nome: "Jay",
+    idade: 63,
+    cpf: "34589756900",
+    email: "pritchettclosets@email.com",
+    telefone: "2020-3030",
+} 
+// adicionando um objeto dentro de outro objeto através da notação de ponto.
+
+cliente.dependentes = {
+    nome: "Gloria",
+    parentesco: "esposa",
+    dataNascimento: "24/09/1971",
+
+}
+
+console.log(cliente)
